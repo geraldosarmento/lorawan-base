e073338135b420c8a9048a40a88d35eb90de13af
geraldosarmento <sir.geraldosarmento@gmail.com>
2024-10-13 20:04:13 -0300

    SimRunner v0.0.2

--- examples/simRunner.py
+++ examples/simRunner.py
@@ -4,10 +4,11 @@ from datetime import datetime
 import os
 import shutil
 import time
+from matplotlib.font_manager import FontProperties
+from scipy.stats import norm
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import copy
 
 # Sample script:  ./src/lorawan/examples/simRunner.py 0
 # Sample generated command: time ./ns3 run "littoral --adrType=ns3::AdrMB --simTime=86400" --quiet
@@ -26,7 +27,7 @@ sideLength      = 10000
 areaCirc        = False
 numPeriods      = 1   #  1.125: whether consider warming time
 #simTime         = numPeriods * 24*60*60     # Não usar tempo menor que 2h (7200s)
-simTime         = 9600 #43200 #14400 #7200
+simTime         = 7200 #43200 #14400 #7200
 pktSize         = 30                      # 0 para usar valor default do módulo
 pktsPerDay      = 144
 appPeriod       = 86400/pktsPerDay
@@ -42,45 +43,26 @@ modoConf        = False   # Caso True, ativa-se o modo confirmado e utiliza-se a
 
 
 # -= Listas, Dicionários e Estruturas de Dados =-
-dimDic          = { 'dim1' : [0], 'dim2' : [0], 'dim3' : [0] }      # Dicionário para armazenar os valores de 3 conjuntos de valores utilizados na simulação
-dimIdDic        = { 'dim1' : "", 'dim2' : "", 'dim3' : "" }
+dimDic          = { 'dim1' : [0], 'dim2' : [0] }      # Dicionário para armazenar os valores de 3 conjuntos de valores utilizados na simulação
+dimIdDic        = { 'dim1' : "", 'dim2' : ""  }
 numEDLst        = [200, 400, 600, 800, 1000]
 MobDic          = {"1.0":"Mobile"} if (mobility) else {"0.0":"Static"}  # "0.5":"Semi-Mobile"
-AdrDic          = {"ns3::AdrMB":"MB-ADR", "ns3::AdrKalman":"M-ADR", "ns3::AdrLorawan":"ADR"} 
+AdrDic          = {"ns3::AdrMB":"MB-ADR"} 
+#AdrDic          = {"ns3::AdrMB":"MB-ADR", "ns3::AdrKalman":"M-ADR", "ns3::AdrLorawan":"ADR"} 
 # Esquemas: "ns3::AdrLorawan":"ADR" "ns3::AdrPlus":"ADR+" "ns3::AdrPF":"PF-ADR"  "ns3::AdrKalman":"M-ADR" "ns3::AdrMB":"MB-ADR"  "ns3::AdrKriging":"K-ADR"  "ns3::AdrEMA":"EMA-ADR" , "ns3::AdrGaussian":"G-ADR" "ns3::AdrFuzzyMB":"FADR-M", "ns3::AdrFuzzyRep":"FL-ADR"
 PcktsDayLst     = [72, 96, 144, 288]  # Esses valores em 'App period' equivalem a 300s,600s,900s e 1200s, respect.
 ModMobLst       = [0, 1, 2]
 EnvironmentLst  = [0, 1, 2]
 cenarioLgd      = 'Tipos de cenário: 0:numED, 1:numED multGw, 2:sideLength, 3:Pkts per day, 4:modMob, 5:classe Veloc'
-#DimDic         = {'nED':'Number of End Devices',  'adrType':'ADR Scheme', 'sideLength':'Side Length', 'appPeriodSecs':'Packets per Day', 'mobModel':'Mobility Model'}
+#metricasLst     = ['PDR', 'EneCon', 'EneEff', 'Latencia','CPSR']
+metricasDic     = {"PDR": "PDR", "EneCon":"Energy consumption (J)", "EneEff":"Energy efficiency (bits/J)", "Latencia":"Uplink latency (s)", "CPSR": "CPSR"}
+#metricasDic     = {"PDR": "PDR", "EneCon":"Energy consumption (J)", "EneEff":"Energy efficiency (bits/J)", "Latencia":"Uplink latency (s)", "PLR_I": "Collision Rate"}
+PLRLst          = ['PLR_I', 'PLR_R', 'PLR_T','PLR_S']
+dimLblDic       = {'numED':'Number of End Devices',  'adrType':'ADR Scheme', 'sideLength':'Side Length', 'appPeriodSecs':'Packets per Day', 'mobModel':'Mobility Model'}
 
-amostras = {
-    "PDR":       [],
-    "PDR_ST":    [],
-    "CPSR":      [],
-    "EneCon":    [],
-    "EneEff":    [],
-    "Latencia" : [],
-}
-
-dfMetricas = {
-    "PDR":       pd.DataFrame(),
-    "PDR_ST":    pd.DataFrame(),
-    "CPSR":      pd.DataFrame(),
-    "EneCon":    pd.DataFrame(),
-    "EneEff":    pd.DataFrame(),
-    "Latencia" : pd.DataFrame()
-}
-
-#if multiGw:
-#    dfMetricasMG = copy.deepcopy(dfMetricas)
-
-dfPLR = {
-    "PLR_I": pd.DataFrame(),
-    "PLR_R": pd.DataFrame(),
-    "PLR_T": pd.DataFrame(),
-    "PLR_S": pd.DataFrame()    
-}
+amostras        = {metric: [] for metric in metricasDic.keys()}
+dfMetricas      = {metric: pd.DataFrame() for metric in metricasDic.keys()} 
+dfPLR           = {metric: pd.DataFrame() for metric in PLRLst} 
 
 
 # -= Valores de referência. Não alterar (!) =-
@@ -121,94 +103,110 @@ devStatus      = outputPath + 'deviceStatus-'
 
 
 
-
 def executarSim(): 
     rodCont = 1   
     numTotRod = 0 
 
     tempoAcum = 0
     cmd = ""    
-    #apagarArqs(outputPath)
+    apagarArqs(outputPath)
     reiniciarEstruturas()
     #reiniciarEstruturasST() 
-    #inicializarDictTempo()        
+    #inicializarDictTempo()
+
+    #print(f"dimDic = \n{dimDic}")       
+    #print(f"dimIdDic = \n{dimIdDic}")       
         
     for mob in MobDic.keys(): 
         for gw in GwDic.keys():
             for dim1 in dimDic['dim1']:
-                for dim2 in dimDic['dim2']:
-                    for dim3 in dimDic['dim3']:
-                        for rep in range(numRep):
-                            numTotRod = len(MobDic)*len(GwDic)*len(dimDic['dim1'])*len(dimDic['dim2'])*len(dimDic['dim3'])*numRep
-                            print("=====================================================================================================================")
-                            print(f"   Ensaio atual: {dimIdDic['dim1']}={dim1} | {dimIdDic['dim2']}={dim2} - NumGw: {gw} - Mobilidade:{'Sim' if (float(mob)>0) else 'Não'} - Rep: {rep+1} - Rodada: {rodCont} de {numTotRod}")
-                            print("=====================================================================================================================")
-                            cmd = ajustarComandoSim(mob, gw, dim1, dim2)
-                            print (f"Comando submetido: {cmd}")
-                            print (f"Executando simulação...")
-                            inicio = time.time()
-                            os.system(cmd)
-                            fim = time.time()
-                            tempoExec = fim - inicio
-                            tempoAcum += tempoExec
-                            agora = datetime.now()  
-                            print(f"\nTempo de execução desta rodada: {round(tempoExec/60,2)} min. ({agora.strftime('%Y-%m-%d %H:%M:%S')})")                             
-                            rodCont += 1
+                for dim2 in dimDic['dim2']:                    
+                    for rep in range(numRep):
+                        numTotRod = len(MobDic)*len(GwDic)*len(dimDic['dim1'])*len(dimDic['dim2'])*numRep
+                        print("===============================================================================================================")
+                        print(f"   Ensaio atual: {dimIdDic['dim1']}={dim1} | {dimIdDic['dim2']}={dim2} - NumGw: {gw} - Mobilidade:{'Sim' if (float(mob)>0) else 'Não'} - Rep: {rep+1} - Rodada: {rodCont} de {numTotRod}")
+                        print("===============================================================================================================")
+                        cmd = ajustarComandoSim(mob, gw, dim1, dim2)
+                        print (f"Comando submetido: {cmd}")
+                        print (f"Executando simulação...")
+                        inicio = time.time()
+                        os.system(cmd)
+                        fim = time.time()
+                        tempoExec = fim - inicio
+                        tempoAcum += tempoExec
+                        agora = datetime.now()  
+                        print(f"\nTempo de execução desta rodada: {round(tempoExec/60,2)} min. ({agora.strftime('%Y-%m-%d %H:%M:%S')})")                             
+                        rodCont += 1
 
-                            atualizarDados(dim1, dim2)
+                        atualizarDados(dim1, dim2)
+            salvarDadosMetricasArq(mob, gw)
+            plotarGraficos(mob, gw)
+            #plotar gráficos (carregando dados dos arquivos)
+            #reiniciar estruturas
+
+
+
 
 def reiniciarEstruturas():
     global dfMetricas
 
     modelo = pd.DataFrame()
-    modelo[dimIdDic['dim1']] = dimDic['dim1']
+    modelo[dimIdDic['dim1']] = dimDic['dim1']  #1a coluna: ensaio dim1 - eixo x dos gráficos
     for d in dimDic['dim2']:
-        modelo[d] = dimDic['dim1']
-        
-   
-    dfMetricas = {
-    "PDR":       modelo,
-    "PDR_ST":    modelo,
-    "CPSR":      modelo,
-    "EneCon":    modelo,
-    "EneEff":    modelo,
-    "Latencia" : modelo,   
-    }
+        modelo[d] = None
 
-    #print(f"dfMetricas = \n {dfMetricas}")
+    for ml in metricasDic.keys():
+        dfMetricas[ml] = modelo.copy()
+   
+    #print(f"REINIC. dfMetricas = \n {dfMetricas}")
     
 
 
-def atualizarDados(dim1, dim2, dim3=None):
+def atualizarDados(dim1, dim2):
     global dfMetricas, amostras
 
     arquivoGP = glPcktCnt + adrType + '.csv'
     arqGP = pd.read_csv(arquivoGP, header=None, sep=' ')   
-   
-    amostras['PDR'].append(arqGP.iloc[0,2])  #Coluna com o PDR
-    amostras['Latencia'].append(arqGP.iloc[0,5])  #Coluna com a Latência
+
+    # Adiciona novos dados às amostras
+    amostras[list(metricasDic.keys())[0]].append(arqGP.iloc[0, 2])  # PDR
     if energiaPorED:
-        amostras['EneCon'].append(arqGP.iloc[0,-1]) #Coluna com a Energ por ED
+        amostras[list(metricasDic.keys())[1]].append(arqGP.iloc[0, -1])  # Energia por ED
     else:
-        amostras['EneCon'].append(arqGP.iloc[0,-2]) #Coluna com a Energ Total
-    #EffEne
-    pacReceb  = arqGP.iloc[:,1]
-    totEneCon = arqGP.iloc[:,-2]
-    amostras['EneEff'].append((pacReceb*pktSize*8) / totEneCon)
+        amostras[list(metricasDic.keys())[1]].append(arqGP.iloc[0, -2])  # Energia Total
+    pacReceb = arqGP.iloc[0, 1]
+    totEneCon = arqGP.iloc[0, -2]
+    effEne = (pacReceb * pktSize * 8) / totEneCon
+    amostras[list(metricasDic.keys())[2]].append(effEne)
+    amostras[list(metricasDic.keys())[3]].append(arqGP.iloc[0, 5])  # Latência
 
     if modoConf:
         arquivoGPC = glPcktCntConf + adrType + '.csv'
         arqGPC = pd.read_csv(arquivoGPC, header=None, sep=' ')   
-        amostras['CPSR'].append(arqGPC.iloc[0,2])  #Coluna com o CPSR
+        amostras['CPSR'].append(arqGPC.iloc[0, 2])  # CPSR
 
-    if (len(amostras['PDR']) == numRep):        
-        #print(f"dfMetricas['PDR'] = \n {dfMetricas['PDR']}")
-        dfMetricas['PDR'].loc[dim1, dim2] = amostras['PDR']
-        print(f"dfMetricas['PDR'] = \n {dfMetricas['PDR']}")
+    # Verifica se temos amostras suficientes para atualizar
+    if len( amostras[list(metricasDic.keys())[0]] ) == numRep:        
+        i = dimDic['dim1'].index(dim1)  # Índice para a linha
 
-        
+        for ml in metricasDic.keys():
+            dfMetricas[ml].at[i, dim2] = amostras[ml].copy()
+            
+        for ml in metricasDic.keys():
+            amostras[ml].clear() 
 
-       
+        #dfMetricas['PDR'].at[i, dim2] = cont
+        #cont += 1
+        '''# Atualiza os DataFrames com cópias das listas
+        for ml in metricasLst:
+            dfMetricas[ml].at[i, dim2] = amostras[ml].copy()  # Usando .copy() para garantir uma nova lista
+
+        # Limpa as amostras para a próxima rodada
+        for ml in metricasLst:
+            amostras[ml].clear()  # Limpa a lista para a próxima rodada'''
+
+        #print(f"dfMetricas = \n{dfMetricas}")
+
     
 
           
@@ -221,16 +219,16 @@ def ajustarLstCenarios(parser):
     tipoCenario = args.arg1
 
     if (tipoCenario == 0 or tipoCenario == 1):
-        dimIdDic['dim1'] = 'Number of EDs'
+        dimIdDic['dim1'] = list(dimLblDic.keys())[0]  #numED
         dimDic['dim1'] = numEDLst if (tipoExecucao == 0) else numEDLst[1:4]
-        dimIdDic['dim2'] = 'ADR scheme'
+        dimIdDic['dim2'] = list(dimLblDic.keys())[1]  #'ADR scheme'
         dimDic['dim2'] = AdrDic.keys() if (tipoExecucao == 0) else list(AdrDic.keys())[:2]
         
         
 
 
 
-def ajustarComandoSim(mob, gw, dim1, dim2, dim3=None):
+def ajustarComandoSim(mob, gw, dim1, dim2 ):
     global numED, adrType, sideLength, pktsPerDay, modMob, minSpeed, maxSpeed
 
     if (tipoCenario == 0 or tipoCenario == 1):
@@ -273,7 +271,81 @@ def ajustarComandoSim(mob, gw, dim1, dim2, dim3=None):
 
     params = " ".join([f"{k}={v}" for k, v in base_params.items()])
     return f"./ns3 run \"littoral {params}\" --quiet"
+
+##### GRÁFICOS ######
+# Plotar gráfico de acordo com alguma métrica específica
+def plotarGraficos(mob, gw):
+    global marcadores
+
+    if (not novaSim):
+        carregarDadosMetricasArq(mob, gw)
     
+    # Obtem um gráfico para cada métrica
+    for metK, metV in metricasDic.items():
+        if (metK == 'CPSR' and not modoConf):
+            continue
+
+        eixo_x = dfMetricas[metK].iloc[:, 0]   
+        dados = dfMetricas[metK].iloc[:, 1:]   # Removendo o primeiro elemento de cada Series (que corresponde ao rótulo da coluna)
+        dfMedia = dados.map(lambda lista: np.mean(lista))
+        dfDP = dados.map(lambda lista: np.std(lista, ddof=1))   
+        
+        z = norm.ppf(areaIC)
+
+        fig, ax = plt.subplots(figsize=(7, 6))  # Definindo o tamanho do gráfico
+            
+        marc = marcadores
+        if not exibirMarc:
+            marc = [' ']
+
+        for i, coluna in enumerate(dfMedia.columns):
+            eixo_y = dfMedia[coluna]
+            desvio = dfDP[coluna]            
+            erro_padrao = desvio / np.sqrt(numRep) * z  # Calcula o erro padrão 
+            cor = corLinhas[i % len(corLinhas)]        
+            
+            plt.errorbar(eixo_x, eixo_y, yerr=erro_padrao, fmt='o', capsize=12, capthick=3, lw=4, color=cor, markersize=2) if barraErro else None
+            plt.plot(eixo_x, eixo_y, linestyle=estilos[i % len(estilos)], marker=marc[i % len(marc)], ms=10, lw=2.8, label=coluna, color=cor, markeredgecolor=corPreenc, mew=1.2)  # Adiciona uma linha para cada coluna
+        
+        plt.xticks(eixo_x, fontsize=tamFonteGraf, fontname=nomeFonte)
+        plt.yticks(fontsize=tamFonteGraf, fontname=nomeFonte)
+        plt.grid(axis='y', linestyle='--')         
+
+        plt.xlabel(dimLblDic[dimIdDic['dim1']], fontsize=tamFonteGraf, fontname=nomeFonte)
+        plt.ylabel(metV, fontsize=tamFonteGraf, fontname=nomeFonte)      
+
+        # Legenda
+        legend_font = FontProperties(family=nomeFonte, style='normal', size=tamFonteGraf-3)
+        if legendaAcima:        
+            plt.legend(prop=legend_font, loc='upper center', bbox_to_anchor=(0.5, 1.2), ncol=len(dfMedia.columns))
+        else:        
+            leg = plt.legend(prop=legend_font)
+            leg.get_frame().set_alpha(0.5)  # Ajustando a transparência da legenda
+
+        plt.tight_layout()
+        plt.savefig(f"{outputPath}Cen{tipoCenario}-{dimIdDic['dim1']}-{metK}-MbltProb{mob}-{gw}Gw.png", bbox_inches='tight')
+        plt.close()
+    
+##### ARQUIVOS ######
+# Função para salvar um DF em um arquivo JSON
+def salvarDadosMetricasArq(mob, gw):
+    for ml in metricasDic.keys():
+        dfMetricas[ml].to_json(f"{outputPath}Cen{tipoCenario}-{dimIdDic['dim1']}-{ml}-MbltProb{mob}-{gw}Gw.json", orient='records')
+
+def carregarDadosMetricasArq(mob, gw):
+    global dfMetricas
+    for ml in metricasDic.keys():
+        dfMetricas[ml] = pd.read_json(f"{outputPath}Cen{tipoCenario}-{dimIdDic['dim1']}-{ml}-MbltProb{mob}-{gw}Gw.json", orient='records')
+
+def apagarArqs(path, extensao=None):
+    try:
+        for arquivo in os.listdir(path):
+            caminho_arquivo = os.path.join(path, arquivo)
+            if os.path.isfile(caminho_arquivo):
+                if extensao is None or arquivo.endswith(extensao):
+                    os.remove(caminho_arquivo)
+    except Exception as e:
+        print(f"Ocorreu um erro ao excluir os arquivos: {e}")
 
 def backupData(path):
     # Remover a barra final, se existir
@@ -311,6 +383,12 @@ def main():
         arq.write(msgFinal)
         arq.close()'''
         backupData(outputPath) if backupOutputDir else None  # Realiza uma cópia do diretório de saída
+    else:                           
+        print("Replotando gráficos...")
+        reiniciarEstruturas() 
+        for mob in MobDic.keys(): 
+            for gw in GwDic.keys():
+                plotarGraficos(mob, gw)
             
 
 if __name__ == '__main__':
